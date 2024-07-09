509d363dfbe97fe8b51e902132c4eb17197077c5
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-09 15:24:53 +0200

    Done

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,23 @@
-module.exports = {
-  checkId,
-  checkPayload,
-}
+const db = require('../../data/db-config')
 
-function checkId(req, res, next) {
-  next()
+async function checkId(req, res, next) {
+  const shipper = await db('shippers').where('shipperid', req.params.id).first()
+  if (!shipper) {
+    next({ status: 404, message: 'that id does not exists'})
+  } else {
+    next()
+  }
 }
 
 function checkPayload(req, res, next) {
-  next()
+  if (!req.body.phone || !req.body.shippername) {
+    next({ status: 422, message: 'Phone and shippername are required'})
+  } else {
+    next()
+  }
+}
+
+module.exports = {
+  checkId,
+  checkPayload,
 }
--- api/shippers/shippers-model.js
+++ api/shippers/shippers-model.js
@@ -1,29 +1,42 @@
 const db = require('../../data/db-config')
 
+async function get() {
+  // this is without knex, but it works. Not a good practice!!
+  // const result = await db.raw('select * from shippers;') 
+  const result = await db('shippers')
+  // to select columns
+  // .select('phone', 'shippername') 
+  return result
+}
+
+async function getById(shipperId) {
+  const result = await db('shippers').where('shipperId', shipperId)
+  return result
+}
+
+async function create(shipper) {
+  const [shipperId] = await db('shippers').insert(shipper)
+  const result = await getById(shipperId)
+  return result
+}
+
+async function update(shipperId, changes) {
+  await db('shippers').update(changes).where('shipperid', shipperId)
+  const result = await getById(shipperId)
+  return result
+}
+
+async function remove(shipperId) {
+  const toBeDeleted = await getById(shipperId)
+  await db('shippers').del().where('shipperid', shipperId)
+  return toBeDeleted
+}
+
+
 module.exports = {
   get,
   getById,
   create,
   update,
   remove,
-}
-
-async function get() {
-  return 'get wired'
-}
-
-async function getById() {
-  return 'getById wired'
-}
-
-async function create() {
-  return 'create wired'
-}
-
-async function update() {
-  return 'update wired'
-}
-
-async function remove() {
-  return 'delete wired'
 }
